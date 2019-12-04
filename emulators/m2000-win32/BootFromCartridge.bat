705c79b0dd28ed321574888b91bf30de469483b8
Dion Olsthoorn <dion.olsthoorn@gmail.com>
2019-12-04 14:25:22 +0100

    Added cartridges!!!! Thanks to cancom from retroforum.nl

---
+++ emulators/m2000-win32/BootFromCartridge.bat
@@ -0,0 +1,7 @@
+@echo off
+if "%~1"=="" (
+  echo Drag-and-drop a .bin cartridge file on 'BootFromCartridge.bat' to start the program in the M2000 emulator directly
+  pause&exit
+)
+cd %~dp0
+m2000.exe -ram 64k -video 1 "%~1"

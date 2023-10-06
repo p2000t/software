64d770b8ac73da82b4c5eb9451df4710b1a3a351
Dion Olsthoorn <dion.olsthoorn@gmail.com>
2023-10-06 22:28:32 +0200

    checked and cleaned up .cas files

--- cassettes/find_duplicates.py
+++ cassettes/find_duplicates.py
@@ -11,19 +11,24 @@ def main():
 
     checksums = []
     paths = [] # store list of file paths
+    duplicates_found = False
 
     for root, dirs, files in os.walk('.'):
         for file in files:
             path = os.path.join(root, file)
-            if os.path.splitext(path)[1].lower() == '.cas':
-                with open(path, 'rb') as f:
+            if os.path.splitext(path)[1].lower() == ".cas":
+                with open(path, "rb") as f:
                     checksum = hashlib.md5(f.read()).hexdigest()
                     if checksum in checksums:
                         otherPath = paths[checksums.index(checksum)]
-                        print('Duplicate .cas files found: "%s" and "%s"' % (path, otherPath))        
+                        print('Duplicate .cas files found: "%s" and "%s"' % (path, otherPath))
+                        duplicates_found = true     
                     else:
                         checksums.append(checksum)
                         paths.append(path)
+
+    if not duplicates_found:
+        print("No duplicates found. You're good to go!") 
 
 if __name__ == '__main__':
     main()    

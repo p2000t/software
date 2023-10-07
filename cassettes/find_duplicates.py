4f52ea8061597a6e4d9a15236d688f5471e6fc1f
Dion Olsthoorn <dion.olsthoorn@gmail.com>
2023-10-07 15:49:41 +0200

    fixed de-dup script to ignore headers

--- cassettes/find_duplicates.py
+++ cassettes/find_duplicates.py
@@ -11,24 +11,28 @@ def main():
 
     checksums = []
     paths = [] # store list of file paths
-    duplicates_found = False
 
     for root, dirs, files in os.walk('.'):
         for file in files:
             path = os.path.join(root, file)
             if os.path.splitext(path)[1].lower() == ".cas":
                 with open(path, "rb") as f:
-                    checksum = hashlib.md5(f.read()).hexdigest()
+                    bytes = bytearray(f.read())
+                    # ignore headers by clearing them
+                    for i in range(len(bytes) - 1):
+                        if i % (256+1024) < 256:
+                            bytes[i] = 0
+                    checksum = hashlib.md5(bytes).hexdigest()
                     if checksum in checksums:
                         otherPath = paths[checksums.index(checksum)]
-                        print('Duplicate .cas files found: "%s" and "%s"' % (path, otherPath))
-                        duplicates_found = true     
+                        print('\n[ERROR] Duplicate .cas files found: "%s" and "%s"' % (path, otherPath))
+                        return
                     else:
+                        print('Checked "%s"' % path)
                         checksums.append(checksum)
                         paths.append(path)
 
-    if not duplicates_found:
-        print("No duplicates found. You're good to go!") 
+    print("\nNo duplicates found. You're good to go!")
 
 if __name__ == '__main__':
     main()    

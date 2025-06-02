243e69b0d893876c7a64ff4808886514ea762ff3
dionoid <dion.olsthoorn@gmail.com>
2025-06-02 22:35:55 +0200

    added numbering in cassette folders

--- cassettes/.support/check_headers.py
+++ cassettes/.support/check_headers.py
@@ -1,10 +1,11 @@
 import os
+import math
 import hashlib
 
 #
 # Purpose
 #
-# Find duplicate .cas files which have different names, but the same MD5 hash
+# Find .cas files with invalid headers
 #
 
 def main():
@@ -20,10 +21,13 @@ def main():
             if os.path.splitext(path)[1].lower() == ".cas":
                 paths.append(path)
     
+    counter = 0
     # check CAS headers
     for path in paths:
-        if not check_cartridge_header(path):
-            print('Invalid CAS header: %s' % path)
+        [valid, prog_type] = check_cartridge_header(path)
+        if not valid:
+            counter += 1
+            print(f'{counter:03d} ({"Single prog" if prog_type == 1 else "Multi prog" }) Fixed invalid CAS header: %s' % path)
 
     print("\n...done!")
 
@@ -36,23 +40,41 @@ def check_cartridge_header(path):
     Returns parsing check (true/false) and list of errors
     """
     with open(path, 'rb') as f:
-        data = f.read()
+        data = bytearray(f.read())
 
     # calculate number of blocks
     nrblocks = len(data) // 0x500
-    prev_recnum = 0
+
+    num_bytes_loc = 0x500 + 0x34
+    num_bytes = int.from_bytes(data[num_bytes_loc:num_bytes_loc+2], byteorder='little')
+    if math.ceil(num_bytes / 1024) == nrblocks:
+        prog_type=1
+    else:
+        prog_type=2
+
+    prev_recnum = 1
+    valid = True
     
     # loop over blocks
     for i in range(nrblocks):
         # check if number of remaining blocks is valid
         recnum = data[i * 0x500 + 0x4F]
-
+        
         # check if the record number is one smaller than previous record number
-        if prev_recnum > 1 and recnum != prev_recnum-1:
-            return False
+        if prev_recnum != 1 and recnum != prev_recnum-1:
+            valid = False
         prev_recnum = recnum
 
-    return True
+    # if not valid and prog_type == 1:
+        
+    #     # fix the header
+    #     for i in range(nrblocks):
+    #         data[i * 0x500 + 0x4F] = nrblocks - i
+    #     # and save the file
+    #     with open(path, 'wb') as f:
+    #         f.write(data)
+
+    return [valid, prog_type]
 
 if __name__ == '__main__':
     main()    
